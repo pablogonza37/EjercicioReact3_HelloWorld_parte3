3bae9568a67f5f9ed07ddc4865147e78b423c0c8
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-22 23:59:43 -0300

    se creo el componente Titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
 import './App.css'
+import Titulo from "./components/Titulo";
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Container>
+      <Titulo></Titulo>
+    </Container>
   )
 }
 
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,9 @@
+const Titulo = () => {
+    return (
+        <h1>
+           Hello 
+        </h1>
+    );
+};
+
+export default Titulo;
