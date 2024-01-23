72ce1fcb23725d51a65e6e71efb3f42e0fd4f5dd
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-23 13:26:32 -0300

    se agrego un state al componente Titulo

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,9 +1,16 @@
+import Button from 'react-bootstrap/Button';
+import { useState } from "react";
+
 const Titulo = (props) => {
-    return (
-        <h1 className="display-1 text-center">
-           Hello {props.mensajeProps}!
-        </h1>
-    );
+    const [mensaje, setMensaje] = useState({ msj: '' });
+  
+
+  return (
+    <article className='text-center'>
+      <h1 className="display-1 text-center">Hello {props.mensajeProps}{mensaje.msj}!</h1>
+      <Button variant="dark" onClick={() => setMensaje({ msj: ' (from changed state)' })}>Click me</Button>
+    </article>
+  );
 };
 
 export default Titulo;
